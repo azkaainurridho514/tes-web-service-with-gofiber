cccbe7282a53a66a4396a6eb1fa0bebd832afbc7
azkaainurridho514 <azkaainurridho514@gmail.com>
2025-11-10 17:08:36 +0700

    tess

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/azkaainurridho514/employee_presence/router"
@@ -11,12 +12,28 @@ import (
 )
 func main() {
 	app := fiber.New()
+
 	app.Use(logger.New())
 	app.Use(cors.New())
+
+	// Routes
 	router.SetupRoutes(app)
-		app.Use(func(c *fiber.Ctx) error {
-	return c.SendStatus(404) 
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.Status(404).JSON(fiber.Map{
+			"status":  "error",
+			"message": "Route not found",
+		})
 	})
+
 	port := os.Getenv("PORT")
-	app.Listen(":" + port)
+	if port == "" {
+		port = "3000" 
+	}
+
+	log.Println("Server running on port:", port)
+
+	if err := app.Listen(":" + port); err != nil {
+		log.Fatal(err)
+	}
 }
